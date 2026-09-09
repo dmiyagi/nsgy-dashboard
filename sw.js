6fa54341faa7282c5d27b3765ff5b3d0902efc30
Danielle Miyagishima, M.D. Ph.D. <dfmiyagishima@gmail.com>
2026-09-08 21:19:57 -0400

    o

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "nsgy-v98";
+const C = "nsgy-v99";
 const SHELL = ["./", "./index.html", "./watch.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
